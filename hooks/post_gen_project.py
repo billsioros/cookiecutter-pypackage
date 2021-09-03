365e0e05448424d1ff438d2eb6c156bbe2b2955a
billsioros <billsioros97@gmail.com>
2021-09-04 02:55:38 +0300

    feat: add `generate_license` task

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,14 +1,18 @@
+import datetime
 import functools
 import json
+import pathlib
 import subprocess
+from urllib.error import URLError
 from urllib.request import urlopen
 
 
-def subprocess_safe(method):
+def transactional(method):
     @functools.wraps(method)
     def wrapper(*args, **kwargs):
         try:
-            return method(*args, **kwargs)
+            for cmd in method(*args, **kwargs):
+                subprocess.run(cmd, check=True)
         except (subprocess.CalledProcessError, FileNotFoundError) as expected_error:
             print(f"Skipping \"{' '.join(args)}\"... ({expected_error})")
         except Exception as unexpected_error:
@@ -17,38 +21,60 @@ def subprocess_safe(method):
     return wrapper
 
 
-def run(cmd):
-    subprocess.run(cmd, check=True)
-
-
 def http_get(url):
     try:
         with urlopen(url) as response:
             return json.loads(response.read())
-    except urllib.error.URLError:
+    except URLError:
         raise ValueError(f"Failed to fetch URL '{url}'")
 
 
-@subprocess_safe
+@transactional
 def install_dependencies():
-    run(["poetry", "shell"])
-    run(["poetry", "install"])
+    yield "poetry", "shell"
+    yield "poetry", "install"
 
 
-@subprocess_safe
+@transactional
 def initialize_repository():
-    run(["git", "init"])
-    run(["git", "add", "."])
-    run(["git", "commit", "-m", "'feat: initial commit'"])
-    run(["git", "remote", "add", "origin", "{{cookiecutter.github_repository}}"])
+    yield "git", "init"
+    yield "git", "add", "."
+    yield "git", "commit", "-m", "'feat: initial commit'"
+    yield "git", "remote", "add", "origin", "{{cookiecutter.github_repository}}"
 
 
-@subprocess_safe
+@transactional
 def install_precommit_hooks():
-    run(["pre-commit", "install", "--install-hooks"])
+    yield "pre-commit", "install", "--install-hooks"
 
 
-JOBS = [install_dependencies, initialize_repository, install_precommit_hooks]
+def generate_license():
+    url = "https://api.github.com/licenses"
 
-for job in JOBS:
-    job()
+    chosen_license = None
+    for available_license in http_get(url):
+        if available_license["key"] == "{{cookiecutter.license}}":
+            chosen_license = available_license
+
+    chosen_license = http_get(chosen_license["url"])
+
+    license_body = chosen_license["body"]
+
+    year = datetime.datetime.utcnow().strftime("%Y")
+
+    license_body = license_body.replace("[year]", f"{year}-{year}")
+    license_body = license_body.replace(
+        "[fullname]", "{{cookiecutter.author}} ({{cookiecutter.github_user}})"
+    )
+
+    with (pathlib.Path.cwd() / "LICENSE") as license_file:
+        license_file.write_text(license_body)
+
+
+for task in [
+    generate_license,
+    install_dependencies,
+    initialize_repository,
+    install_precommit_hooks,
+]:
+    task()

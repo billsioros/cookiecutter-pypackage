824c3f4c7a3a8808f3f0118aafe4b474a5cbac02
billsioros <billsioros97@gmail.com>
2021-09-09 23:21:01 +0300

    chore(main): remove code coverage

--- {{cookiecutter.project_name}}/tests/test_{{cookiecutter.package_name}}.py
+++ {{cookiecutter.project_name}}/tests/test_{{cookiecutter.package_name}}.py
@@ -1,6 +1,6 @@
 """Tests concerning the `{{ cookiecutter.package_name }}` module."""
 
-from {{ cookiecutter.package_name }}.{{ cookiecutter.package_name }} import factorial
+from {{cookiecutter.package_name}}.{{cookiecutter.package_name}} import factorial
 
 
 def test_{{ cookiecutter.package_name }}():
